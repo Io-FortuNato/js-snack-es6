b90ad47c0941df07e8db9b76d1462528160e7099
io.FortuNato <nomefortunato60@gmail.com>
2026-02-18 00:51:41 +0100

    start work

---
+++ main.js
@@ -0,0 +1,8 @@
+// array di oggetti (bici)
+const bici = [
+    { nome: "Bianchi", peso: 7 },
+    { nome: "Rossi", peso: 8 },
+    { nome: "Verdi", peso: 9 },
+    { nome: "Brancaleone", peso: 6 },
+];
+
