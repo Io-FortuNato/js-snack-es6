d340507a91911accda4fa770c3401cad9c582c25
io.FortuNato <nomefortunato60@gmail.com>
2026-02-18 00:55:19 +0100

    add array and calculator

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const bici = [
     { nome: "Brancaleone", peso: 6 },
 ];
 
+// bici più leggera 
+let biciLight = bici[0];
+for (let i = 1; i < bici.length; i++) {
+    if (bici[i].peso < biciLight.peso) {
+        biciLight = bici[i];
+    }
+}
+
