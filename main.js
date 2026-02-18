520d3856456de3783976267ba2c752f7ad80cf10
io.FortuNato <nomefortunato60@gmail.com>
2026-02-18 01:16:56 +0100

    add function random numbers

---
+++ main.js
@@ -0,0 +1,17 @@
+const squadre = [
+    { nome: "Realmadrid", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+];
+
+//funzione numero random 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() *
+(max - min + 1)) + min;
+}
+
+// sostituzione dei numeri 0
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i].puntiFatti =
+}
